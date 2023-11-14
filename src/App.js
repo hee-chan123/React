311b82d0a4019ec83ebd3e2f05f23e8df829a785
hee-chan123 <144683260+hee-chan123@users.noreply.github.com>
2023-11-14 10:53:36 +0900

    11-14

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+function Movie(){
+    return <h1>I like potato</h1>
+}
+
 function App() {
-    return <div>Hello!!!!</div>;
+    return (
+        <div>
+        <h1>Hello!!!!</h1>
+        <Movie />
+        <Movie />
+        <Movie />
+        <Movie />
+        </div>
+        );
 }
 
 export default App;
